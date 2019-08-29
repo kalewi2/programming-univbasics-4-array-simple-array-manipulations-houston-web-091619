08133ba456f53f60aa9d3fe49cdb5c9f7b08c115
Kim Lewis <kalewi2@gmail.com>
2019-08-29 15:46:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,8 +14,8 @@ def pop_with_args(array)
   array.pop(2)
 end
 
-def  using_unshift(array)
-  array.unshift()
+def  using_unshift(array, string)
+  array.unshift(string)
 end
 
 def using_shift_with_args(array)
